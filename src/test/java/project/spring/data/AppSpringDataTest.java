323b18f58fa8788853edb3686e8b8d1ee5ff35f4
Eder Campos Ribeiro <edercribeiro@gmail.com>
2020-10-10 08:50:15 -0300

    Inserting on DataBase

--- src/test/java/project/spring/data/AppSpringDataTest.java
+++ src/test/java/project/spring/data/AppSpringDataTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import project.spring.data.dao.InterfaceSpringDataUser;
+import project.spring.data.model.UserSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
@@ -17,7 +18,19 @@ public class AppSpringDataTest {
 	
 	@Test
 	public void testInsert() {
-		System.out.println("Spring has initiate successfuly!");
+		
+		UserSpringData userSpringData = new UserSpringData();
+		
+		userSpringData.setEmail("edercribeiro@gmail.com");
+		userSpringData.setAge(37);
+		userSpringData.setLogin("test 123");
+		userSpringData.setPassword("123");
+		userSpringData.setName("Mazzoccante");
+		
+		interfaceSpringDataUser.save(userSpringData);
+		
+		System.out.println("Inserted Users -> " + interfaceSpringDataUser.count());
+		
 	}
 
 }
